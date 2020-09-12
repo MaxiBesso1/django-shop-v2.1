61f361ffa677407057d9f86e0ca898d63b7a6127
maxi besso <maxibesso11@gmail.com>
2020-09-12 16:54:29 -0300

    Images3

--- products_catalog/settings.py
+++ products_catalog/settings.py
@@ -127,10 +127,11 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 LOGIN_REDIRECT_URL = reverse_lazy("admin",args=("productos",))
 LOGOUT_REDIRECT_URL = reverse_lazy("index")
 
